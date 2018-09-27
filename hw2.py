2ef10614a6a15a3bd5cb658f65e790c7b4861710
Spencer Turkel <spencer@palmbeachsoftware.com>
2018-09-27 14:14:49 -0400

    Reformat verifyProof

--- hw2.py
+++ hw2.py
@@ -1,13 +1,14 @@
 import random
 
 
+# noinspection PyPep8Naming
 def verifyProof(P):
-    """F: A string which is an S-expression of a well-formed Fitch-style
+    """
+    :param P: A string which is an S-expression of a well-formed Fitch-style
     proof.
-    Returns either a string or integer:
-    “E” – If P was not well-formed (GRADUATE STUDENTS ONLY)
-    “I” – If P was well-formed, but not a valid proof,
-    “V” – If P was well-formed, and a valid proof.
+    :return: Returns either:
+        “I” – If P was well-formed, but not a valid proof,
+        “V” – If P was well-formed, and a valid proof.
     """
     to_return = random.choice(["E", "I", "V"])
     if to_return == "i":
